dcdb80b26ee9823aa5d0a0cb604870972dca026b
BergerMarcin <marcin.berger@wp.pl>
2026-01-23 00:08:34 +0100

    style: run prettier

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-console.log("Starting around-world-api")
-console.log("NODE VERSION", process.version)
-console.log("NODE VERSION", process.env)
+console.log('Starting around-world-api')
+console.log('NODE VERSION', process.version)
+console.log('NODE VERSION', process.env)
